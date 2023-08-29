99dada21fc186b237c6126f16547abcadd9f2d27
Ava's DAO <support@avasdao.org>
2023-08-29 05:35:57 -0400

    Add Formidable form/upload handler.

---
+++ web/server/routes/v1/asset.post.ts
@@ -0,0 +1,91 @@
+/* Import modules. */
+import formidable from 'formidable'
+import moment from 'moment'
+import PouchDB from 'pouchdb'
+import { sha256 } from '@nexajs/crypto'
+
+/* Initialize databases. */
+const logsDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/logs`)
+const rainmakerProfilesDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/rainmaker_profiles`)
+const rainmakerTxsDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/rainmaker_txs`)
+
+export default defineEventHandler(async (event) => {
+    /* Initialize locals. */
+    let address
+    let body
+    let campaign
+    let campaignid
+    let fields
+    let files
+    let form
+    let profiles
+    let receivers
+    let response
+    let txidem
+    let assetPkg
+
+    /* Initialize Formidable library. */
+    form = formidable({ multiples: true })
+    // form = formidable()
+
+    response = await form.parse(event.node.req)
+        .catch(err => console.error(err))
+    console.log('RESPONSE', response)
+
+    if (!response?.length) {
+        return null
+    }
+
+    return response[0]
+
+    campaign = body.campaign
+    campaignid = campaign.id
+
+    receivers = body.receivers
+
+    txidem = body.txidem
+
+    profiles = receivers.map(_receiver => {
+        let profileid
+
+        profileid = sha256(`${campaignid}:${_receiver.address}`)
+
+        return profileid
+    })
+
+    for (let i = 0; i < profiles.length; i++) {
+        const profileid = profiles[i]
+
+        const profile = await rainmakerProfilesDb
+            .get(profileid)
+            .catch(err => console.error(err))
+        // console.log('PROFILE-1', profile)
+
+        /* Validate profile. */
+        if (profile) {
+            profile.txs.push(txidem)
+            profile.updatedAt = moment().unix()
+            // console.log('PROFILE-2', profile)
+
+            response = await rainmakerProfilesDb
+                .put(profile)
+                .catch(err => console.error(err))
+            // console.log('UPDATE PROFILE', response)
+        }
+    }
+
+    txPkg = {
+        _id: txidem,
+        campaignid,
+        profiles,
+        createdAt: moment().unix(),
+    }
+    // console.log('TX PKG', txPkg)
+
+    response = await rainmakerTxsDb
+        .put(txPkg)
+        .catch(err => console.error(err))
+    // console.log('RESPONSE', response)
+
+    return txidem
+})
