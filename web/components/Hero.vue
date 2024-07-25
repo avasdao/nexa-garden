614de0c6b732889816c9ed51273e4234a31f1a52
Ava's DAO <support@avasdao.org>
2024-07-25 10:25:26 -0400

    Update media + libs and refactor.

--- web/components/Hero.vue
+++ web/components/Hero.vue
@@ -106,7 +106,7 @@ const props = defineProps({
 
             <div class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
                 <div class="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-                    <img src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="App screenshot" width="2432" height="1442" class="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10" />
+                    <img src="~/assets/hero-nft-gallery.jpg" alt="App screenshot" width="2432" height="1442" class="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10" />
                 </div>
             </div>
         </div>
